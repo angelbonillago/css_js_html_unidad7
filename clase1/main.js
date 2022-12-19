46b5f49fb125d67ec41e094368f7005ec24e5a8d
angelbonillago <angel15b98g@gmail.com>
2022-12-19 10:03:41 -0500

    clase1 finalizada

--- clase1/main.js
+++ clase1/main.js
@@ -1,5 +1,19 @@
 function changeColor(newColor) {
     var elem = document.getElementById('texto');//Tomo el id 'texto'
+
     elem.style.color = newColor;//ejecuto el cambio de color que reciba por el parametro 'newColor'
+ 
 }
+
+function cambiarLabel() {
+    console.log("llego aqui");
+    document.querySelector('#label1').innerHTML="Otro texto nuevo";  //ID
+    document.querySelector('.parrafotexto').style.color = 'red'; //class
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    alert('')
+});
+
+
 
